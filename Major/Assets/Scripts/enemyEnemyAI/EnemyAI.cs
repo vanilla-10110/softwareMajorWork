19b71cf0c75b1316b417e7c3d0aa8c59e6ed192f
vanilla-10110 <106131238+vanilla-10110@users.noreply.github.com>
2023-07-24 09:40:07 +1000

    fucked around but didnt find out

--- Major/Assets/Scripts/enemyEnemyAI/EnemyAI.cs
+++ Major/Assets/Scripts/enemyEnemyAI/EnemyAI.cs
@@ -72,26 +72,26 @@ public class EnemyAI : MonoBehaviour
     }
     private void AttackPlayer()
     {
-        //ensure enemy doesnt move 
+        //Make sure enemy doesn't move
         agent.SetDestination(transform.position);
 
         transform.LookAt(player);
 
         if (!alreadyAttacked)
         {
-            //attack code here
+            ///Attack code here
             Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
             rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
             rb.AddForce(transform.up * 8f, ForceMode.Impulse);
-            //
+            ///End of attack code
+
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
     }
     private void ResetAttack()
     {
-        alreadyAttacked = false; 
-
+        alreadyAttacked = false;
     }
 
     /*public void TakeDamage(int damage)
