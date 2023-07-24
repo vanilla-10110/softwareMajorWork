9d0d09f70eb60af21af8d47498a79c1243b5cc96
vanilla-10110 <106131238+vanilla-10110@users.noreply.github.com>
2023-07-24 14:34:49 +1000

    pell book

--- Major/Assets/Scripts/enemyEnemyAI/EnemyAI.cs
+++ Major/Assets/Scripts/enemyEnemyAI/EnemyAI.cs
@@ -81,8 +81,8 @@ public class EnemyAI : MonoBehaviour
         {
             ///Attack code here
             Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
-            rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+            rb.AddForce(transform.forward * 6f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 6f, ForceMode.Impulse);
             ///End of attack code
 
             alreadyAttacked = true;
