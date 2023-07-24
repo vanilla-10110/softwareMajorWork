45bb00d9e3a453068deb07af3256d364cd1b5883
RealFreditor <freditor12345@gmail.com>
2023-07-24 14:41:11 +1000

    Made body snap
    Finished body snap and updated the player in all scenes.

--- Major/Assets/Scripts/bodySnap.cs
+++ Major/Assets/Scripts/bodySnap.cs
@@ -12,6 +12,10 @@ public class bodySnap : MonoBehaviour
     
     void FixedUpdate()
     {
-        
+        transform.position = target.position + Vector3.up * offset.y 
+            + Vector3.ProjectOnPlane(target.right,Vector3.up).normalized * offset.x
+            + Vector3.ProjectOnPlane(target.forward, Vector3.up).normalized * offset.z;  
+
+        transform.eulerAngles = new Vector3 (0, target.eulerAngles.y, 0);
     }
 }
