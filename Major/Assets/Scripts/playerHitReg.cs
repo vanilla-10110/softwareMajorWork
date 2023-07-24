3ce95c7055f25d14e416837ccf336867a4cf6187
RealFreditor <freditor12345@gmail.com>
2023-07-24 15:59:01 +1000

    zoe:
    spell book shoot now and be good to a degree and the bullets are now fireballs and tis alll good and all that

---
+++ Major/Assets/Scripts/playerHitReg.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHitReg : MonoBehaviour
+{
+    public float enemyHealth = 100f;
+    public float forceMagnitude = 8f;
+    public Rigidbody rb;
+    WeaponScript enemyWeapon;
+
+    public GameObject ragDoll;
+    public GameObject enemy;
+    public Rigidbody ragDollRB;
+
+    void Start()
+    {
+        GameObject weaponGameObject = GameObject.FindGameObjectWithTag("Weapon");
+        if (weaponGameObject != null)
+        {
+            enemyWeapon = weaponGameObject.GetComponent<WeaponScript>();
+        }
+        else
+        {
+            Debug.LogError("No weapon GameObject found with the 'Weapon' tag.");
+        }
+    }
+
+
+    private void Update()
+    {
+        if (enemyHealth <= 0)
+        {
+            Debug.Log("wow me be deds");
+            enemyHealth = 100f;
+            enemy.SetActive(false);
+            ragDoll.SetActive(true);
+            ragDoll.transform.position = enemy.transform.position;
+            ragDoll.transform.rotation = enemy.transform.rotation;
+            ragDollRB.AddForce(-transform.forward * forceMagnitude, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Weapon"))
+        {
+            float playerDamage = enemyWeapon.damage;
+            TakeDamage(playerDamage);
+            Debug.Log("Dealt " + playerDamage + " Damage.");
+        }
+    }
+
+    private void TakeDamage(float damage)
+    {
+        enemyHealth -= damage;
+        if (enemyHealth <= 0)
+        {
+            enemy.SetActive(false);
+            ragDoll.SetActive(true);
+            ragDoll.transform.position = enemy.transform.position;
+            ragDoll.transform.rotation = enemy.transform.rotation;
+            ragDollRB.AddForce(-transform.forward * forceMagnitude, ForceMode.Impulse);
+        }
+    }
+}
