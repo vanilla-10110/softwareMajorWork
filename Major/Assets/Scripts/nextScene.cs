dae6a416e07469f32522464d9fac6cd8378c2cb1
RealFreditor <freditor12345@gmail.com>
2023-07-26 09:28:06 +1000

    item transfer
    Got the scene to deload and the player and such became a child of the new scene, but i couldnt interact with anything. Decided to change to having all the scenes in 1 and teleporting between them. Tested teleporting with items in hand and pockets and they come too so less work for me!

--- Major/Assets/Scripts/nextScene.cs
+++ Major/Assets/Scripts/nextScene.cs
@@ -14,51 +14,19 @@ public class nextScene : MonoBehaviour
     public GameObject player;
     public GameObject weapons;
 
-    IEnumerator OnTriggerEnter(Collider collision)
+
+
+
+    private void OnTriggerEnter(Collider collision)
     {
+
         if (collision.GetComponent<Collider>() == headCollider)
         {
-            
 
-            // Load the new scene additively
-            AsyncOperation sceneLoadOperation = SceneManager.LoadSceneAsync(targetSceneIndex, LoadSceneMode.Additive);
-            sceneLoadOperation.allowSceneActivation = false;
+        player.transform.position = new Vector3(10, 10, 10);
 
-            while (sceneLoadOperation.progress < 0.9f)
-            {
-                yield return null;
-               
-            }
-
-            sceneLoadOperation.allowSceneActivation = true; // Activate the scene
-
-            // Get the current active scene dynamically
-            Scene currentActiveScene = SceneManager.GetActiveScene();
-
-            // Check if the 'main room' scene is loaded
-            Scene mainRoomScene = SceneManager.GetSceneByName("main room");
-            if (mainRoomScene.isLoaded)
-            {
-                // Set the 'main room' scene as active
-                SceneManager.SetActiveScene(mainRoomScene);
-                Debug.Log("Scene 'main room' set as active");
-            }
-            else
-            {
-                Debug.Log("Scene 'main room' is not loaded.");
-            }
-
-            // Unload the previous scene
-            SceneManager.UnloadSceneAsync(currentActiveScene);
-
-            // Move objects and set the new scene as the active scene
-            Scene targetScene = SceneManager.GetSceneByBuildIndex(targetSceneIndex);
-            SceneManager.MoveGameObjectToScene(itemManager, targetScene);
-            SceneManager.MoveGameObjectToScene(player, targetScene);
-            SceneManager.MoveGameObjectToScene(weapons, targetScene);
-            SceneManager.SetActiveScene(targetScene);
-
-            Debug.Log("Scene unloaded successfully");
         }
     }
+
+
 }
