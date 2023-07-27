49bd416a3ae9b7163dc23623df68374c4f0212f6
RealFreditor <freditor12345@gmail.com>
2023-07-27 10:42:22 +1000

    Setup scene transitions
    Portals between scenes now work, items go with you. very frustrating to get to work but it does now.

--- Major/Assets/Scripts/portal/startGame.cs
+++ Major/Assets/Scripts/portal/startGame.cs
@@ -1,26 +1,36 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-
-
-
 public class startGame : MonoBehaviour
 {
-
     public Collider headCollider;
+    public GameObject player;
 
-    private void OnTriggerEnter(Collider collision)
+    
+    public GameObject prevStage;
+
+    public Transform tpPos;
+
+
+
+
+
+
+    private IEnumerator OnTriggerEnter(Collider collision)
     {
-        
+
         if (collision.GetComponent<Collider>() == headCollider)
         {
             
-            SceneManager.LoadScene("stage one");
+
+            yield return new WaitForSeconds(3);
+
+            player.transform.position = tpPos.position;
+            prevStage.SetActive(false);
 
         }
     }
-   
+
+
 }
