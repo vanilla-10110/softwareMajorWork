f45472c3922479fad4eb83fa78961d37223551e2
vanilla-10110 <106131238+vanilla-10110@users.noreply.github.com>
2023-07-31 12:35:44 +1000

    did boss things

---
+++ Major/Assets/Scripts/bossHitReg.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossHitReg : MonoBehaviour
+{
+    public float enemyHealth = 100f;
+    public float forceMagnitude = 8f;
+    public Rigidbody rb;
+    WeaponScript playerWeapon;
+
+    public GameObject ragDoll;
+    public GameObject enemy;
+    public GameObject endPortal; 
+    public Rigidbody ragDollRB;
+
+    void Start()
+    {
+        GameObject weaponGameObject = GameObject.FindGameObjectWithTag("Weapon");
+        if (weaponGameObject != null)
+        {
+            playerWeapon = weaponGameObject.GetComponent<WeaponScript>();
+        }
+        else
+        {
+            Debug.LogError("No weapon GameObject found with the 'Weapon' tag.");
+        }
+    }
+
+
+    private void Update()
+    {
+        if (enemyHealth <= 0)
+        {
+            Debug.Log("wow me be deds");
+            enemyHealth = 100f;
+            enemy.SetActive(false);
+            ragDoll.SetActive(true);
+            ragDoll.transform.position = enemy.transform.position;
+            ragDoll.transform.rotation = enemy.transform.rotation;
+            ragDollRB.AddForce(-transform.forward * forceMagnitude, ForceMode.Impulse);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Weapon"))
+        {
+            float playerDamage = playerWeapon.damage;
+            TakeDamage(playerDamage);
+            Debug.Log("Dealt " + playerDamage + " Damage.");
+        }
+    }
+
+    private void TakeDamage(float damage)
+    {
+        enemyHealth -= damage;
+        if (enemyHealth <= 0)
+        {
+            enemy.SetActive(false);
+            ragDoll.SetActive(true);
+            ragDoll.transform.position = enemy.transform.position;
+            ragDoll.transform.rotation = enemy.transform.rotation;
+            ragDollRB.AddForce(-transform.forward * forceMagnitude, ForceMode.Impulse);
+            endPortal.SetActive(true); 
+        }
+    }
+}
